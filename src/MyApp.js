2c758ce4794761b2952423e49881c0d78be71758
Tyler <tyler.r.herzog@gmail.com>
2022-09-26 10:16:12 -0700

    Created form for custom name-job input

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,30 +1,11 @@
 import React, {useState} from 'react'
 import Table from './Table'
+import Form from './Form';
 
 
-// const characters = [
-
-// ];
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: 'Charlie',
-      job: 'Janitor',
-    },
-    {
-      name: 'Mac',
-      job: 'Bouncer',
-    },
-    {
-      name: 'Dee',
-      job: 'Aspring actress',
-    },
-    {
-      name: 'Dennis',
-      job: 'Bartender',
-    },
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
@@ -36,6 +17,7 @@ function MyApp() {
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form />
     </div>
   )
 }
